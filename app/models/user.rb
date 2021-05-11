19879eaa538bd5bdf14939822fd15e1bd0579416
pountzas <nikos@pountzas.gr>
2021-05-12 00:37:08 +0300

    Change user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 8 }
   validates :email, presence: true, length: { minimum: 10 }
-  validates :passwords, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
